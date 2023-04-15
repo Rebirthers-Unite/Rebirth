39d64d456c56a6c60cfc4bb21ade2a50f2ee2ec1
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-04-15 18:24:07 +0300

    added create method to users controller

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  # POST to create new user
   def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -7,4 +18,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def user_params
+      params.permit(:fullname, :email, :phone, :role, :password)
+    end
 end
