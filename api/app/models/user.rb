4990076d936bdcaae1485c50159ea87db8d9d68f
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-04-15 18:35:28 +0300

    updated user.rb

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -6,4 +6,9 @@ class User
   field :phone, type: Integer
   field :role, type: String
   field :password_digest, type: String
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with:URI::MailTo::EMAIL_REGEXP}
+  validates :password, presence: true, length: { minimum:6 }, on: :create
 end
