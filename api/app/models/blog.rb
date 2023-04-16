64b06b9fbc38ce7737da54d02e601a216c5e8a2e
ismahaneyy <liciousmayrah@gmail.com>
2023-04-16 22:35:52 +0300

    created blog model with the required columns

---
+++ api/app/models/blog.rb
@@ -0,0 +1,8 @@
+class Blog
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :title, type: String
+  field :description, type: String
+  field :author, type: String
+  field :user_id, type: Integer
+end
