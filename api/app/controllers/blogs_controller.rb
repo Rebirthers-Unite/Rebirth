ce4b0f7b954c861275966498b01df6861919cdc6
ismahaneyy <liciousmayrah@gmail.com>
2023-04-16 22:58:41 +0300

    added the pseudocode for the before_action method

---
+++ api/app/controllers/blogs_controller.rb
@@ -0,0 +1,5 @@
+class BlogsController < ApplicationController
+    # Define a `before_action` method 
+    # In the `before_action` method, define a `set_blog` method that sets the `@blog` instance variable to the blog object with the specified `:id` parameter.
+    # The `set_blog` method should be called before the `show`, `update`, and `destroy` actions 
+end
