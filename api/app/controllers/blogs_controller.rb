82dd13d0a874e3fb4425d943b269bca3c6f397f0
ismahaneyy <liciousmayrah@gmail.com>
2023-04-16 22:59:22 +0300

    defined before action method

--- api/app/controllers/blogs_controller.rb
+++ api/app/controllers/blogs_controller.rb
@@ -2,4 +2,7 @@ class BlogsController < ApplicationController
     # Define a `before_action` method 
     # In the `before_action` method, define a `set_blog` method that sets the `@blog` instance variable to the blog object with the specified `:id` parameter.
     # The `set_blog` method should be called before the `show`, `update`, and `destroy` actions 
+
+    before_action :set_blog, only: [:show, :update, :destroy]
+
 end
