08828eac841d6394082fd9a21abec506736b3697
ismahaneyy <liciousmayrah@gmail.com>
2023-04-16 14:18:16 +0300

    added the pseudocode for create method

--- api/app/controllers/survivors_controller.rb
+++ api/app/controllers/survivors_controller.rb
@@ -13,6 +13,12 @@ class SurvivorsController < ApplicationController
         render json: @survivor
     end
     
+      # CREATE
+      # Define the create method
+      # Create a new instance of the Survivor model with the input parameters
+      # If the new survivor is valid, save it to the database and return a success message with the new survivor object
+      # If the new survivor is not valid, return an error message with the validation errors
+      
     
 
 end
