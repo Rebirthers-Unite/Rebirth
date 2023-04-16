515024f4f2b4f707b3c4c3d95c58cc41c68821f7
ismahaneyy <liciousmayrah@gmail.com>
2023-04-16 22:48:26 +0300

    added the required validations

--- api/app/models/blog.rb
+++ api/app/models/blog.rb
@@ -5,4 +5,7 @@ class Blog
   field :description, type: String
   field :author, type: String
   field :user_id, type: Integer
+
+  validates :title, :description, :author, :user_id, presence: true
+  validates :description, length: { minimum: 50 }
 end
