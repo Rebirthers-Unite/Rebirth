1a0a6d7710f952ad2fe1b06bb1788f572ee06aa9
ismahaneyy <liciousmayrah@gmail.com>
2023-04-17 09:55:21 +0300

    commented two lines

--- api/app/models/blog.rb
+++ api/app/models/blog.rb
@@ -4,7 +4,9 @@ class Blog
   field :title, type: String
   field :description, type: String
   field :author, type: String
-  field :user_id, type: Integer
+  # field :user_id, type: Integer
+
+  belongs_to :user
 
   validates :title, :description, :author, :user_id, presence: true
   validates :description, length: { minimum: 50 }
