e5fa21eb886f67a929cb9fce71daaf8fcf1f41d8
Osorabrian <osorabrian@gmail.com>
2023-04-18 11:09:06 +0300

    edit landing page folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,16 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/LandingPage/Hero/Hero'
-import About from './components/LandingPage/About/About'
-import Survivors from './components/LandingPage/Survivors/Survivors'
+import { LandingPage } from './components/LandingPage/LandingPage'
 import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
    <Navbar/>
-   <Hero/>
-   <About/>
-   <Survivors/>
+   <Routes>
+      <Route element={<LandingPage/>} path='/' />
+   </Routes>
    <Footer/>
    </div>
   )
