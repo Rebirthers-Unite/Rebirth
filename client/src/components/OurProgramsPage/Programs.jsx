3efdbe97e4af830993b74401ff26a43b68771503
Osorabrian <osorabrian@gmail.com>
2023-04-18 11:30:15 +0300

    add bootstrap links

--- client/src/components/OurProgramsPage/Programs.jsx
+++ client/src/components/OurProgramsPage/Programs.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { ProgramsCard } from './ProgramsCard'
 import './programs.css'
 
 export const Programs = () => {
   return (
     <div id='our-programs-page'>
-
+        <h1>Our Programs</h1>
+        <ProgramsCard/>
     </div>
   )
 }
--- client/src/components/OurProgramsPage/ProgramsCard.jsx
+++ client/src/components/OurProgramsPage/ProgramsCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import avatar from '../../assets/face.jpeg'
 
 export const ProgramsCard = () => {
   return (
-    <div className='card'>
-
+    <div className='card w-100'>
+        <img src={avatar} alt='card-image' className='card-img-top'/>
+        <div className='card-body'>
+          <h3 className='card-title'>Rebirth of a Queen Safe House</h3>
+          <p className='card-text'>We run an operational safehouse that rescues survivors, teenage mothers and children 
+          from all parts of the country and refugees who have been trafficked and end up facing sexual and gender-based violence. 
+          Our commitment is to continue keeping the safehouse secure, safe and healing space for all to heal and thrive. </p>
+        </div>
     </div>
   )
 }
