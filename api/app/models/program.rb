b6b798269dc376fceea84218eb65940c50f2e292
Sammy-CK <sammycherono2696@gmail.com>
2023-04-18 18:40:37 +0300

    Added the program model

---
+++ api/app/models/program.rb
@@ -0,0 +1,11 @@
+class Program
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Paperclip
+
+  field :title, type: String
+  field :description, type: String
+  # field :image, type: Attachment
+
+  has_mongoid_attached_file :image
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :programs
   # Routes for CRUD operations staff
   resources :staffs
   
