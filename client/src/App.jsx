4a7e34945b331dc567b8d93d8095592615358919
Osorabrian <osorabrian@gmail.com>
2023-04-18 11:15:36 +0300

    add routes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,20 @@ import viteLogo from '/vite.svg'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import { LandingPage } from './components/LandingPage/LandingPage'
+import { Programs } from './components/OurProgramsPage/Programs'
 import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
    <Navbar/>
-   <Routes>
-      <Route element={<LandingPage/>} path='/' />
-   </Routes>
+    <Routes>
+        <Route element={<LandingPage/>} path='/' />
+        <Route element={<Programs/>} path='programs' />
+    </Routes>
    <Footer/>
    </div>
-  )
+)
 }
 
 export default App
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
-import { FiUser } from 'react-icons/fi';// Import the FiUser icon from React Icons library
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
-// import useNavbarStore from '../../store';
+import { FiUser } from 'react-icons/fi';// Import the FiUser icon from React Icons Linkbrary
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='text-white cursor-pointer flex justify-between items-center sticky h-24 max-w-[1240px] mx-auto px-4'>
       <h1 className='w-full text-3xl font-bold text-[#ffcc3d] flex'> REBIRTH </h1>
       <ul className='hidden md:flex'>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> Home </li>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> About </li>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> Programs </li>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> Contact </li>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> Support </li>
-        <li className='p-4 hover:text-yellow-300 hover:font-bold'> <FiUser size={20}/> </li>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold' to='/'> Home </Link>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold'> About </Link>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold' to='/programs'> Programs </Link>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold'> Contact </Link>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold'> Support </Link>
+        <Link className='p-4 hover:text-yellow-300 hover:font-bold'> <FiUser size={20}/> </Link>
       </ul>
     </div>
   )
