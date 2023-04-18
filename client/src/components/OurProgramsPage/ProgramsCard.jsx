30d7d6bad6cd3fcca1b010f06b7ef633ec421ba0
Osorabrian <osorabrian@gmail.com>
2023-04-18 15:31:02 +0300

    change font family

--- client/src/components/OurProgramsPage/ProgramsCard.jsx
+++ client/src/components/OurProgramsPage/ProgramsCard.jsx
@@ -7,14 +7,10 @@ export const ProgramsCard = () => {
     <div className='card row mb-5 px-0' id='card-div'>
         <img src={avatar} alt='card-image' className='col-2' id='card-image'/>
         <div className='col-10' id='card-content'>
-          <h3 className='card-title mt-4'> Rebirth of a Queen Safe House </h3>
-          <p className='card-text mb-3' style={{width: '650px'}}>We run an operational safehouse that rescues survivors, teenage mothers and children 
+          <h3 className='card-title mt-4 text-3xl font-serif'> Rebirth of a Queen Safe House </h3>
+          <p className='card-text mb-3 font-sans' style={{width: '650px'}}>We run an operational safehouse that rescues survivors, teenage mothers and children 
           from all parts of the country and refugees who have been trafficked and end up facing sexual and gender-based violence. 
           Our commitment is to continue keeping the safehouse secure, safe and healing space for all to heal and thrive. </p>
         </div>
         <div>
-         <Link to='/donate' className='btn btn-warning' style={{width: '200px'}}>Donate</Link>
-        </div>
-    </div>
-  )
-}
+         <Link to='/donate' className='btn btn-warning' style={{width: '200px'}}>Donate<>
