8d4da7cdd12bea754737139a95eb08a182d772ff
Michelle_Mungai <chellymungai@gmail.com>
2023-04-18 10:14:19 +0300

    Amends

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
