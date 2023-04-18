f3cf374e85ea536738750061173618a4cef895a9
Osorabrian <osorabrian@gmail.com>
2023-04-18 11:03:57 +0300

    edit landing page folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Survivors from './components/Survivors/Survivors'
+import Hero from './components/LandingPage/Hero/Hero'
+import About from './components/LandingPage/About/About'
+import Survivors from './components/LandingPage/Survivors/Survivors'
 import Footer from './components/Footer/Footer'
 
 function App() {
