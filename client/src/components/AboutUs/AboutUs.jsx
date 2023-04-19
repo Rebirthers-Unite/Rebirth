14aadf26b60a44fad5872eb6cdf3aa74ce77b2a5
Osorabrian <osorabrian@gmail.com>
2023-04-19 20:49:23 +0300

    config vite

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import './About.css'
-// import whoWeAre from '../../assets/who we are.JPG'
+import whoWeAre from '../../assets/who we are.JPG'
 
 export const AboutUs = () => {
   return (
     <div id='about-us-page'>
-        <h1 className='text-3xl font-bold font-serif' id='title'>Who We Are</h1>
+        <p className='text-5xl font-bold font-serif' id='our-story'>Our Story</p>
 
-        <div id='who-we-are' className='row'>
-          <p className='font-sans'> 
+          <p className='font-sans' id='who-we-are-content'> 
             We are a community-based organization that champions for the empowerment of survivors of sexual /gender-based
-            violence and human trafficking through healing, mentorship, education and empowerment.<br></br> Founded in 2019, Rebirth of a Queen
-            has been in a commitment to create a safe space for survivors and the vulnerable in the community to be the voices.<br></br> 
-            Our Target group has and still remains to be young women ,girls, teenage Mothers and survivors of sexual and gender-based violence
+            violence and human trafficking through healing, mentorship, education and empowerment. Founded in 2019, Rebirth of a Queen
+            has been in a commitment to create a safe space for survivors and the vulnerable in the community to be the voices. 
+            Our Target group has and still remains to be young women ,girls, teenage Mothers and survivors of sexual and gender-based violence.
           </p>
-          {/* <img src={whoWeAre} alt='who we are'/> */}
-        </div>
 
-        <div>
-          
-        </div>
+        <img src={whoWeAre} alt='who we are'/>
         
     </div>
   )
