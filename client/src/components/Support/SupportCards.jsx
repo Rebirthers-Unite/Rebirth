2c6bc2331cdae6919108b03ee923d8f767dd3f68
Osorabrian <osorabrian@gmail.com>
2023-04-19 06:10:15 +0300

    create support cards component

--- client/src/components/Support/SupportCards.jsx
+++ client/src/components/Support/SupportCards.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import {FaSchool} from 'react-icons/fa'
+import {GiTakeMyMoney} from 'react-icons/gi'
+import {HiSpeakerphone} from 'react-icons/hi'
+import {BsHouseHeartFill} from 'react-icons/bs'
 
 export const SupportCards = () => {
   return (
-    <div className='row'>
+    <div className='row' id='support-cards' style={{textAlign: 'center'}}>
 
-        <div className='card col-3'>
-            <FaSchool id='school'/>
-            <p>Educate the vulnerable</p>
+        <div className='card'>
+            <FaSchool className='icon'/>
+            <p>Educating the vulnerable</p>
         </div>
 
-        <div className='card col-3'>
-
+        <div className='card'>
+            <GiTakeMyMoney className='icon'/>
+            <p>To equip victims with skills that can help in job marketing</p>
         </div>
 
-        <div className='card col-3'>
-
+        <div className='card'>
+            <HiSpeakerphone className='icon'/>
+            <p>Mentorship to raise awareness on mental health</p>
         </div>
 
-        <div className='card col-3'>
-
+        <div className='card'>
+            <BsHouseHeartFill className='icon'/>
+            <p>Operational safehouse that rescues victims from all parts of the country </p>
         </div>
-
 
     </div>
   )
