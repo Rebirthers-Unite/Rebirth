34c60110a5a6c2e6b56e11436e5fd0fb3689d793
Osorabrian <osorabrian@gmail.com>
2023-04-19 21:58:06 +0300

    format about us page

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -14,7 +14,47 @@ export const AboutUs = () => {
             Our Target group has and still remains to be young women ,girls, teenage Mothers and survivors of sexual and gender-based violence.
           </p>
 
-        <img src={whoWeAre} alt='who we are'/>
+          <div className='row'>
+            <div className='col-6'>
+              <img src={whoWeAre} alt='who we are' id='our-story-image'/>
+            </div>
+            <div className='col-6 mt-5'>
+              <p className='text-5xl font-serif'>Our Mission</p>
+              <p className='mt-3 font-sans' id='mission-text'>To advocate and create safe spaces for survivors of sexual/ gender-based violence and human 
+                trafficking among vulnerable groups in the community through sustainable rescues, shelters, 
+                mentorship and empowerment. </p>
+            </div> 
+          </div>
+
+
+          <div className='row'>
+            <div className='col-6 mt-5' id='vision-div'>
+              <p className='text-5xl font-serif' >Our Vision</p>
+              <p className='mt-3 font-sans' id='vision-text'>A frontier organization in ending sexual/ gender-based violence and human trafficking in the community </p>
+            </div> 
+            <div className='col-6'>
+              <img src={whoWeAre} alt='who we are' id='our-vision-image'/>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-6'>
+              <img src={whoWeAre} alt='who we are' id='our-story-image'/>
+            </div>
+            <div className='col-6 mt-5'>
+              <p className='text-5xl font-serif' >Our Values</p>
+              <p className='mt-3 font-sans' id='values-text'>As a young team, we take pride in the values of our organization 
+              that makes us who we are and the impact we intend to make in the community. </p>
+              <ul className='list-disc' id='values-list'>
+                <li>Authenticity</li>
+                <li>Compassion</li>
+                <li>Integrity</li>
+                <li>Accountability</li>
+                <li>Commitment</li>
+                <li>Inclusivity</li>
+              </ul>
+            </div> 
+          </div>          
         
     </div>
   )
