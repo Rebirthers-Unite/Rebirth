dec7f5f782ea2ea888d84760dbd844cbee8b62a8
Osorabrian <osorabrian@gmail.com>
2023-04-19 12:47:33 +0300

    add donate buttons

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './support.css'
 import { SupportCards } from './SupportCards'
+import happy from '../../assets/again.jpeg'
 
 export const Support = () => {
   return (
     <div id='support-page'>
-        <h1 id='title'>Donate</h1>
-        <h1 className='text-3xl font-serif' style={{textAlign: 'center'}}>Your donation will support...</h1>
+        <img src={'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=731&q=80'} alt='' id='donate-image'/>
+        <h1 id='title' className='text-5xl font-bold'>Donate</h1>
+        <h1 className='text-3xl font-serif' style={{textAlign: 'center', marginTop: '120px'}}>Your donation will support...</h1>
         <SupportCards/>
+        <h1 className='text-3xl font-serif' style={{textAlign: 'center', marginTop: '80px'}}>You can donate through</h1>
+
+        <div className='row' style={{textAlign:'center', padding: '60px'}}>
+
+          <div className='col-6'>
+            <a href='' type='button' className='btn btn-warning '>Go Fund Me</a>
+          </div>
+
+          <div className='col-6'>
+          <a href='https://www.mchanga.africa/fundraiser/44234' type='button' className='btn btn-primary'>Mchanga</a>
+          </div>
+
+        </div>
+        
+
     </div>
   )
 }
