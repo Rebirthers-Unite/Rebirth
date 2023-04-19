c49c78a1a94f7268aa39df6114b3a8b69883f494
Osorabrian <osorabrian@gmail.com>
2023-04-19 05:30:45 +0300

    create support folder

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './About.css'
+// import whoWeAre from '../../assets/who we are.JPG'
 
 export const AboutUs = () => {
   return (
     <div id='about-us-page'>
-        <h1 className='text-3xl font'>Who We Are</h1>
-        <p> 
-        Rebirth of a Queen is a community-based organization that champions for the empowerment of survivors of sexual /gender-based
-        violence and human trafficking through healing, mentorship, education and empowerment . Founded in 2019, Rebirth of a Queen
-        has been in a commitment to create a safe space for survivors and the vulnerable in the community to be the voices. 
-        Our Target group has and still remains to be young women ,girls, teenage Mothers and survivors of sexual and gender-based violence
-        </p>
+        <h1 className='text-3xl font-bold font-serif' id='title'>Who We Are</h1>
+
+        <div id='who-we-are' className='row'>
+          <p className='font-sans'> 
+            We are a community-based organization that champions for the empowerment of survivors of sexual /gender-based
+            violence and human trafficking through healing, mentorship, education and empowerment.<br></br> Founded in 2019, Rebirth of a Queen
+            has been in a commitment to create a safe space for survivors and the vulnerable in the community to be the voices.<br></br> 
+            Our Target group has and still remains to be young women ,girls, teenage Mothers and survivors of sexual and gender-based violence
+          </p>
+          {/* <img src={whoWeAre} alt='who we are'/> */}
+        </div>
+
+        <div>
+          
+        </div>
+        
     </div>
   )
 }
