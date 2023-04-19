e5763bcbd503b5232881f526d3ff5719c591fb72
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-04-19 14:05:34 +0300

    added fuctionality to login.jsx

--- client-admin/src/forms/Login.jsx
+++ client-admin/src/forms/Login.jsx
@@ -12,6 +12,46 @@ import { Link } from 'react-router-dom';
 import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 
 const Login = () => {
+
+  const setToken = useAuthStore((state) => state.setToken);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { username, password };
+    fetch('http://127.0.0.1:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const jwt = data.jwt 
+        setToken(jwt)
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+
+
 	const loginPageTexts = {
 		welcomeMessage: 'Welcome Back',
 		forgotPassword: 'Forgot Password?',
@@ -32,7 +72,7 @@ const Login = () => {
 			<FormControl w={'50%'} m={'0 auto'} as={'form'}>
 				<HStack p={'1rem'}>
 					<EmailIcon fontSize={'3rem'} color="purple.400" />
-					<Input type="email" placeholder="email or phone number" required />
+					<Input type="email" placeholder="email or phone number" value={email} onChange={handleUsernameChange} required />
 				</HStack>
 				<HStack p={'1rem'}>
 					<LockIcon fontSize={'3rem'} color="purple.400" />
