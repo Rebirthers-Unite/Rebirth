582122c37a330969a74ffa7579529eb814dc7e00
Osorabrian <osorabrian@gmail.com>
2023-04-19 14:19:18 +0300

    format support.jsx

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -10,15 +10,17 @@ export const Support = () => {
         <h1 id='title' className='text-5xl font-bold'>Donate</h1>
         <h1 className='text-3xl font-serif' style={{textAlign: 'center', marginTop: '120px'}}>Your donation will support...</h1>
         <SupportCards/>
-        <h1 className='text-3xl font-serif' style={{textAlign: 'center', marginTop: '80px'}}>You can donate through</h1>
+        <h1 className='text-5xl font-serif' style={{textAlign: 'center', marginTop: '80px'}}>You can donate through</h1>
 
         <div className='row' style={{textAlign:'center', padding: '60px'}}>
 
           <div className='col-6'>
+            <p>Gofund Me</p><br></br>
             <a href='' type='button' className='btn btn-warning '>Go Fund Me</a>
           </div>
 
           <div className='col-6'>
+            <p>Mchanga</p><br></br>
           <a href='https://www.mchanga.africa/fundraiser/44234' type='button' className='btn btn-primary'>Mchanga</a>
           </div>
 
