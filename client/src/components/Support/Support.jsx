928fd68df96c04c4ad55f50e4727698ec58993a5
Osorabrian <osorabrian@gmail.com>
2023-04-19 06:17:07 +0300

    edit support.css file

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -6,7 +6,7 @@ export const Support = () => {
   return (
     <div id='support-page'>
         <h1 id='title'>Donate</h1>
-        <h1>Your donation will support</h1>
+        <h1 className='text-3xl font-serif' style={{textAlign: 'center'}}>Your donation will support...</h1>
         <SupportCards/>
     </div>
   )
--- client/src/components/Support/SupportCards.jsx
+++ client/src/components/Support/SupportCards.jsx
@@ -6,24 +6,24 @@ import {BsHouseHeartFill} from 'react-icons/bs'
 
 export const SupportCards = () => {
   return (
-    <div className='row' id='support-cards' style={{textAlign: 'center'}}>
+    <div className='row mt-5' id='support-cards' style={{textAlign: 'center'}}>
 
-        <div className='card'>
+        <div className='card shadow support'>
             <FaSchool className='icon'/>
             <p>Educating the vulnerable</p>
         </div>
 
-        <div className='card'>
+        <div className='card shadow support'>
             <GiTakeMyMoney className='icon'/>
             <p>To equip victims with skills that can help in job marketing</p>
         </div>
 
-        <div className='card'>
+        <div className='card shadow support'>
             <HiSpeakerphone className='icon'/>
             <p>Mentorship to raise awareness on mental health</p>
         </div>
 
-        <div className='card'>
+        <div className='card shadow support'>
             <BsHouseHeartFill className='icon'/>
             <p>Operational safehouse that rescues victims from all parts of the country </p>
         </div>
