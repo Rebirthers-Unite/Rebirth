ddfeda09b76d6a828b8855ea7720033e12ddd330
Osorabrian <osorabrian@gmail.com>
2023-04-19 10:28:16 +0300

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import { BlogPage } from './components/BlogPage/BlogPage.jsx'
 import {ViewBlogPage} from './components/ViewBlogs/ViewBlogPage.jsx'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-      <ViewBlogPage/>
+    <Routes>
+      <Route element={<ViewBlogPage/>} />
+    </Routes>
   )
 }
 
