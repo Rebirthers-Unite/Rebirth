be37290cdc996eaf453163b02359a2812c2671c4
Osorabrian <osorabrian@gmail.com>
2023-04-21 10:38:39 +0300

    format fonts

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ import study from '../../assets/study.JPG'
 export const AboutUs = () => {
   return (
     <div id='about-us-page'>
-        <p className='text-5xl font-bold font-serif' id='our-story'>Our Story</p>
+        <p className='text-5xl font-serif' id='our-story'>Our Story</p>
 
           <p className='font-sans' id='who-we-are-content'> 
             We are a community-based organization that champions for the empowerment of survivors of sexual /gender-based
