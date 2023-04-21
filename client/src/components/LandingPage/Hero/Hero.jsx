850d212be852b281859cb332d97a73b89e2ed271
Osorabrian <osorabrian@gmail.com>
2023-04-21 08:15:56 +0300

    styling landing page

--- client/src/components/LandingPage/Hero/Hero.jsx
+++ client/src/components/LandingPage/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
 	return (
-		<div className='text-yellow-300 bg-[url("./assets/stop.jpeg")] bg-center bg-cover hover:font-bold'>
+		<div className='text-yellow-300 bg-[url("../../../assets/")] bg-center bg-cover hover:font-bold'>
 			<div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
 				<h1 className='md:text-7xl sm:text-6xl text-4xl md:py-6'>
 					REBIRTH OF A QUEEN
