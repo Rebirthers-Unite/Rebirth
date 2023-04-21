305d76380c7e4a007a5aabfe236b5ac7c2a7e2b4
Osorabrian <osorabrian@gmail.com>
2023-04-21 09:40:11 +0300

    format contact page

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './contact.css'
+import {BsSend} from 'react-icons/bs'
 import { Container, Row, Col } from 'react-bootstrap'
 
 function Contact() {
   return (
 		<div id='contact-page'>
 		<div id='div-content'>
-			<Row className='mb-5 mt-3' >
-				<Col lg='8'>
-					<h1 className='display-4 mb-4'>Contact Us</h1>
-				</Col>
-			</Row>
+			<div className='mb-5 mt-3' >
+					<h1 className='text-5xl mb-5' style={{textAlign:'center'}}>Contact Us</h1>
+			</div>
 			<Row className='sec_sp'>
 				<Col lg='5' className='mb-5'>
 					<strong className='color_sec py-4'>Get in touch</strong>
@@ -24,35 +23,30 @@ function Contact() {
 					</address>
 				</Col>
 				<Col lg='7' className='d-flex align-items-center'>
-					<form className='contact_form w-100'>
-						<Row>
-							<Col lg='6' className='form-group'>
-								<input
-									className='form-control'
-									id='name'
-									name='name'
-									placeholder='Name'
-									type='text'
-								/>
-							</Col>
-							<Col lg='6' className='form-group'>
-								<input
-									className='form-control'
-									id='email'
-									name='email'
-									placeholder='Email'
-									type='email'
-								/>
-							</Col>
-						</Row>
-                        <textarea className='form-control' id='message' name='message' placeholder='Message' rows='5'></textarea>
-                        <br/>
-                        <Row>
-                            <Col lg='12' className='form-group'>
-                                <button className='btn sc_btn' type='submit'> Send </button>
-                            </Col>
-                        </Row>
-					</form>
+					<form id='contacts-form'>
+					<div className="row mb-3">
+
+						<div className="col-6">
+							<label className="form-label">Your Name</label><br></br>
+							<input type="text" placeholder="Enter your name." className='form-control' required onChange={e => setName(e.target.value)}/>
+						</div>
+
+						<div className="col-6">
+							<label className='form-label'>Email Address</label><br></br>
+							<input type="email" placeholder="Enter your email address." className='form-control' required onChange={e => setEmail(e.target.value)}/>
+						</div>
+
+					</div>
+
+					<div className="row mb-3">
+						<label className="form-label">Your Message</label>
+						<textarea type="text" rows='3' placeholder='Enter your message.' className='form-control' required onChange={e => setMessage(e.target.value)}/>
+					</div>
+
+					<div style={{textAlign: 'center'}}>
+					<button type="submit" id='send-button'>Send Message  <i className="fa-regular fa-paper-plane"></i></button> 
+					</div>		
+				</form>
 				</Col>
 			</Row>
 			</div>
