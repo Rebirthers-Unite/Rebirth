cf0a601189b03b044a4e6b5e25834763121141c3
Osorabrian <osorabrian@gmail.com>
2023-04-23 18:08:18 +0300

    make contact page responsive

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -8,27 +8,27 @@ function Contact() {
 		<div id='contact-page'>
 		<div id='div-content'>
 			<div className='mb-5 mt-3' >
-					<h1 className='text-5xl mb-5 font-serif' style={{textAlign:'center'}}>Contact Us</h1>
-					<h5 style={{textAlign: 'center'}} className='text-3xl'>Got a question or proposal, or just want to say hello? Go ahead.</h5>
+					<h1 className='text-5xl mb-5 font-serif' style={{textAlign:'center'}} id='contact-title'>Contact Us</h1>
+					<h5 style={{textAlign: 'center'}} className='text-3xl' id='contact-welcome'>Got a question or proposal, or just want to say hello? Go ahead.</h5>
 			</div>
 					<form id='contacts-form'>
 					<div className="row mb-3">
 
 						<div className="col-6">
 							<label className="form-label">Your Name</label><br></br>
-							<input type="text" placeholder="Enter your name." className='form-control' required onChange={e => setName(e.target.value)}/>
+							<input type="text" placeholder="Enter your name." className='form-control' required onChange={e => setName(e.target.value)} id='name'/>
 						</div>
 
 						<div className="col-6">
 							<label className='form-label'>Email Address</label><br></br>
-							<input type="email" placeholder="Enter your email address." className='form-control' required onChange={e => setEmail(e.target.value)}/>
+							<input type="email" placeholder="Enter your email address." className='form-control' required onChange={e => setEmail(e.target.value)} id='email'/>
 						</div>
 
 					</div>
 
 					<div className="row mb-3">
 						<label className="form-label">Your Message</label>
-						<textarea type="text" rows='3' placeholder='Enter your message.' className='form-control' required onChange={e => setMessage(e.target.value)}/>
+						<textarea type="text" rows='3' placeholder='Enter your message.' className='form-control' required onChange={e => setMessage(e.target.value)} id='message'/>
 					</div>
 
 					<div style={{textAlign: 'center'}}>
