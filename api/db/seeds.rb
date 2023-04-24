305c886b1a908ca89ed345c57bb07c99ed7544ad
Sammy-CK <sammycherono2696@gmail.com>
2023-04-24 09:39:00 +0300

    Added seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,27 +1,152 @@
-# Create some survivors
-Survivor.create(name: "John Doe", age: 25, date_of_birth: "1998-05-12 00:00:00", organization: "Red Cross", contact: 1234567890, date_of_entry: "2022-01-01 00:00:00", date_of_exit: "2022-06-30 00:00:00", guardian_name: "Jane Doe", contact_of_guardian: 9876543210)
-Survivor.create(name: "Mary Smith", age: 30, date_of_birth: "1993-08-21 00:00:00", organization: "UNICEF", contact: 9876543210, date_of_entry: "2022-02-15 00:00:00", date_of_exit: "2022-08-14 00:00:00", guardian_name: "John Smith", contact_of_guardian: 1234567890)
-Survivor.create(name: "David Lee", age: 20, date_of_birth: "2002-02-10 00:00:00", organization: "Doctors Without Borders", contact: 2345678901, date_of_entry: "2022-03-01 00:00:00", date_of_exit: "2022-09-30 00:00:00", guardian_name: "Emily Lee", contact_of_guardian: 3456789012)
+puts "start"
 
 # Create a user to associate with the blogs
-user1 = User.create(name: "John Doe", email: "john@example.com", role: "admin", password: "password")
-user2 = User.create(name: "Jane Smith", email: "jane@example.com", role: "editor", password: "password")
-user3 = User.create(name: "David Lee", email: "david@example.com", role: "user", password: "password")
+user1 = User.create(name: "John Doe", email: "john333@example.com", role: "admin", password: "password")
+user2 = User.create(name: "Jane Smith", email: "jane323@example.com", role: "editor", password: "password")
+user3 = User.create(name: "David Lee", email: "david2133@example.com", role: "user", password: "password")
+
+# Create some survivors
+Survivor.create!(
+  name: "John Doe",
+  date_of_birth: Date.new(2000, 1, 1),
+  organization: "Red Cross",
+  organization_contact: "Jane Smith",
+  organization_email: "janesmith@example.com",
+  guardian_name: "Bob Johnson",
+  contact_of_guardian: "555-555-5555",
+  level_of_education: "High School",
+  school: "Central High School",
+  children: "None",
+  social_asset_building: "Mentorship Program",
+  date_of_entry: Date.new(2021, 1, 1),
+  date_of_exit: Date.new(2021, 6, 30),
+)
+
+Survivor.create!(
+  name: "Jane Smith",
+  date_of_birth: Date.new(2005, 5, 5),
+  organization: "Save the Children",
+  organization_contact: "John Doe",
+  organization_email: "johndoe@example.com",
+  guardian_name: "Mary Johnson",
+  contact_of_guardian: "555-555-5555",
+  level_of_education: "Middle School",
+  school: "East Middle School",
+  children: "None",
+  social_asset_building: "Art Therapy",
+  date_of_entry: Date.new(2021, 2, 1),
+  date_of_exit: Date.new(2021, 7, 31),
+)
+
+Survivor.create!(
+  name: "Bob Johnson",
+  date_of_birth: Date.new(1998, 12, 31),
+  organization: "UNICEF",
+  organization_contact: "Jane Smith",
+  organization_email: "janesmith@example.com",
+  guardian_name: "John Doe",
+  contact_of_guardian: "555-555-5555",
+  level_of_education: "College",
+  school: "State University",
+  children: "None",
+  social_asset_building: "Job Training",
+  date_of_entry: Date.new(2020, 1, 1),
+  date_of_exit: Date.new(2020, 12, 31),
+)
 
 # Create some blogs
-# Blog.create(title: "The Art of Programming", description: "Programming is a skill that requires dedication and hard work. It takes years of practice to become a master.", author: "Jane Smith", user: user1)
-# Blog.create(title: "Why Ruby on Rails is Awesome", description: "Ruby on Rails is a powerful web development framework that allows developers to create complex web applications quickly and easily.", author: "John Doe", user: user2)
-# Blog.create(title: "The Benefits of Test-Driven Development", description: "Test-driven development is a development approach where developers write tests before writing the code. This helps ensure that the code is correct and reduces the likelihood of bugs.", author: "David Lee", user: user3)
+Blog.create!(
+  title: "First Blog Post",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien vel bibendum bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel bibendum sapien.",
+  author: "John Doe",
+  date: Date.today,
+  user: user1,
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Blog.create!(
+  title: "Second Blog Post",
+  body: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed euismod, sapien vel bibendum bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel bibendum sapien.",
+  author: "Jane Smith",
+  date: Date.today - 1,
+  user: user2,
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Blog.create!(
+  title: "Third Blog Post",
+  body: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed euismod, sapien vel bibendum bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel bibendum sapien.",
+  author: "Bob Johnson",
+  date: Date.today - 2,
+  user: user3,
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
 
 # Create staff
-Staff.create(name: "John Smith", email: "john.smith@example.com", phone_number: "555-555-5555", position: "Program Manager", gender: "Male")
-Staff.create(name: "Jane Doe", email: "jane.doe@example.com", phone_number: "555-555-5555", position: "Social Worker", gender: "Female")
-Staff.create(name: "David Lee", email: "david.lee@example.com", phone_number: "555-555-5555", position: "Case Manager", gender: "Male")
-Staff.create(name: "Maria Rodriguez", email: "maria.rodriguez@example.com", phone_number: "555-555-5555", position: "Counselor", gender: "Female")
+Staff.create!(
+  name: "John Doe",
+  email: "johndoe@example.com",
+  phone_number: "555-555-5555",
+  position: "Manager",
+  gender: "Male",
+)
+
+Staff.create!(
+  name: "Jane Smith",
+  email: "janesmith@example.com",
+  phone_number: "555-555-5555",
+  position: "Sales Associate",
+  gender: "Female",
+)
+
+Staff.create!(
+  name: "Bob Johnson",
+  email: "bobjohnson@example.com",
+  phone_number: "555-555-5555",
+  position: "Customer Service Representative",
+  gender: "Male",
+)
+
+# Create Gallery
+Gallery.create!(
+  title: "Nature",
+  description: "Beautiful landscapes and wildlife",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Gallery.create!(
+  title: "Cityscapes",
+  description: "Stunning views of urban environments",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Gallery.create!(
+  title: "Abstract",
+  description: "Artistic and creative images",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
 
 # Create Program
-# Program.create(title: "Rebirth of a Queen Safe House", description: "We run an operational safehouse that rescues survivors, teenage mothers and children from all parts of the country and refugees who have been trafficked and end up facing sexual and gender-based violence. Our commitment is to continue keeping the safehouse secure, safe and healing space for all to heal and thrive.
-#     ", image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"))
-# Program.create(title: "Rebirth Elimisha Project", description: "We provide formal and informal holistic education to vulnerable groups at the safehouse and the community. Our intention is to continue providing education to the vulnerable in the society and champion for equality in access to education.", image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"))
-# Program.create(title: "Raising Authentic Voices ", description: "This is a mentorship program for the teenagers and young people in the community to raise awareness on mental health, sexual reproductive health and rights, and sexual and gender-based violence. Through the talks we also create safe spaces for young people to express themselves in authenticity.
-#     ", image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"))
+Program.create!(
+  title: "Rebirth of a Queen Safe House",
+  description: "We run an operational safehouse that rescues survivors, teenage mothers and children from all parts of the country and refugees who have been trafficked and end up facing sexual and gender-based violence. Our commitment is to continue keeping the safehouse secure, safe and healing space for all to heal and thrive.",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Program.create!(
+  title: "Rebirth Elimisha Project",
+  description: "We provide formal and informal holistic education to vulnerable groups at the safehouse and the community. Our intention is to continue providing education to the vulnerable in the society and champion for equality in access to education.",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Program.create!(
+  title: "Raising Authentic Voices",
+  description: "This is a mentorship program for the teenagers and young people in the community to raise awareness on mental health, sexual reproductive health and rights, and sexual and gender-based violence. Through the talks we also create safe spaces for young people to express themselves in authenticity.",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
+
+Program.create!(
+  title: "Economic Empowerment",
+  description: "This is a program that targets young women , girls and survivors who we have rescued from the safehouse to access skills that can help them in job marketing. Some of the skills we offer are fashion design. The empowerment program is to champion dignified and fulfilling work to young women to thrive and be part of the decision makers in the economic empowerment space.",
+  image: File.new("#{Rails.root}/app/assets/images/youth_empowerment.JPG"),
+)
