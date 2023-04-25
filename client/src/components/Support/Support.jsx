22ed3b60789741f58ee7a2bf20677b6e7eee40cb
Michelle_Mungai <chellymungai@gmail.com>
2023-04-25 15:33:06 +0300

    Made the support page responsive

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -1,38 +1,64 @@
-import React from 'react'
-import './support.css'
-import { SupportCards } from './SupportCards'
-import happy from '../../assets/again.jpeg'
+import React from 'react';
+import { SupportCards } from './SupportCards';
+import happy from '../../assets/again.jpeg';
 
-export const Support = () => {
-  return (
-    <div id='support-page'>
-        <img src={'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=731&q=80'} alt='' id='donate-image'/>
-        <h1 id='title' className='text-7xl font-serif'>Donate</h1>
-        <h1 className='text-3xl font-serif' style={{textAlign: 'center', marginTop: '120px'}}>Your donation will support...</h1>
-        <SupportCards/>
-        <h1 className='text-5xl font-serif' style={{textAlign: 'center', marginTop: '80px'}}>You can donate through</h1>
+const Support = () => {
+	return (
+		<div id='support-page' className='bg-purple-200 sm:m-[-20px] md:m-0 pb-[100px]'>
+			<img
+				src={
+					'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=731&q=80'
+				}
+				alt=''
+				id='donate-image'
+				className='w-full h-auto -mt-96 -mb-12'
+			/>
+			<h1
+				id='title'
+				className='text-7xl font-serif text-white absolute top-32 left-1/2 transform -translate-x-1/2'>
+				Donate
+			</h1>
+			<h1 className='text-3xl pb-4 pt-4 font-serif mt-24 text-center text-black'>
+				Your donation will support...
+			</h1>
+			<SupportCards />
+			<h1 className='text-5xl font-serif mt-4 text-center text-black'>
+				You can donate through
+			</h1>
 
-        <div className='row' style={{textAlign:'center', padding: '60px'}}>
+			<div className='grid sm:grid-cols-1 sm:justify-center sm:align-center md:grid-cols-3 text-center md:py-8 md:pr-48'>
+				<div className='col-4'>
+					<a href='https://gofund.me/bc18e7b5'>
+						<img
+							src='https://p.kindpng.com/picc/s/151-1514121_go-fund-me-hd-png-download.png'
+							alt='gofundme logo'
+							id='gofundme-logo'
+							className='hover:scale-105 sm:w-40 sm:h-30 md:h-30 md:h-40 md:pb-6 md:w-72 sm:mt-4 sm:align-center'
+						/>
+					</a>
+				</div>
 
-          <div className='col-4 '>
-            <a href='https://gofund.me/bc18e7b5'>
-              <img src='https://p.kindpng.com/picc/s/151-1514121_go-fund-me-hd-png-download.png' alt='gofundme logo' id='gofundme-logo' className='hover:scale-105'/>
-            </a>
-          </div>
+				<div className='col-2'>
+					<p id='or-text' className='text-3xl font-serif pl-16'>
+						or
+					</p>
+				</div>
 
-          <div className='col-2'>
-            <p id='or-text' className='text-3xl font-serif'>or</p>
-          </div>
+				<div className='col-6'>
+					<a href='https://www.mchanga.africa/fundraiser/44234'>
+						<img
+							src={
+								'https://tadamon.community/uploads/images/mchanga_logo_1574fa55-a36a-4b2e-b4b5-c317c5b1cff6.png?v=63819070917'
+							}
+							alt='mchanga'
+							id='mchanga-logo'
+							className='hover:scale-105 w-40'
+						/>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-          <div className='col-6'>
-            <a href='https://www.mchanga.africa/fundraiser/44234'>
-              <img src={'https://tadamon.community/uploads/images/mchanga_logo_1574fa55-a36a-4b2e-b4b5-c317c5b1cff6.png?v=63819070917'} alt='mchanga' id='mchanga-logo' className='hover:scale-105'/>
-            </a>
-          </div>
-
-        </div>
-        
-
-    </div>
-  )
-}
+export default Support;
--- client/src/components/Support/SupportCards.jsx
+++ client/src/components/Support/SupportCards.jsx
@@ -6,28 +6,29 @@ import {BsHouseHeartFill} from 'react-icons/bs'
 
 export const SupportCards = () => {
   return (
-    <div className='row mt-5' id='support-cards' style={{textAlign: 'center'}}>
+		<div className='grid sm:grid-cols-1 align-center justify-center me-5 mb-5 px-0 md:grid-cols-4 pt-0 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
+			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
+				<FaSchool className='icon' />
+				<p>Educating the vulnerable</p>
+			</div>
 
-        <div className='card shadow support'>
-            <FaSchool className='icon'/>
-            <p>Educating the vulnerable</p>
-        </div>
+			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
+				<GiTakeMyMoney className='icon' />
+				<p>To equip victims with skills that can help in job marketing</p>
+			</div>
 
-        <div className='card shadow support'>
-            <GiTakeMyMoney className='icon'/>
-            <p>To equip victims with skills that can help in job marketing</p>
-        </div>
+			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
+				<HiSpeakerphone className='icon' />
+				<p>Mentorship to raise awareness on mental health</p>
+			</div>
 
-        <div className='card shadow support'>
-            <HiSpeakerphone className='icon'/>
-            <p>Mentorship to raise awareness on mental health</p>
-        </div>
-
-        <div className='card shadow support'>
-            <BsHouseHeartFill className='icon'/>
-            <p>Operational safehouse that rescues victims from all parts of the country </p>
-        </div>
-
-    </div>
-  )
+			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
+				<BsHouseHeartFill className='icon' />
+				<p>
+					Operational safehouse that rescues victims from all parts of the
+					country{' '}
+				</p>
+			</div>
+		</div>
+	);
 }
