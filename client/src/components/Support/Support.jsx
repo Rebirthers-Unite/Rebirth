aed955097c1dfd1a08c8d0b15f849e7d3605614d
Michelle_Mungai <chellymungai@gmail.com>
2023-04-28 14:12:22 +0300

    Made it responsive

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -11,40 +11,41 @@ export const Support = () => {
 				}
 				alt=''
 				id='donate-image'
-				className='w-full h-auto -mt-96 -mb-12'
+				className='w-full md:h-[760px] -mt-96 '
 			/>
 			<h1
 				id='title'
-				className='text-7xl font-serif text-white absolute top-32 left-1/2 transform -translate-x-1/2'>
+				className='text-7xl font-serif text-white absolute sm:top-32 md:top-60 left-1/2 transform -translate-x-1/2'>
 				Donate
 			</h1>
-			<h1 className='text-3xl pb-4 pt-4 font-serif mt-24 text-center text-black'>
+			<h1 className='text-3xl pb-4 pt-14 font-serif mt-4 text-center text-black'>
 				Your donation will support...
 			</h1>
-			<SupportCards/>
-			<h1 className='text-5xl font-serif mt-4 text-center text-black'>
+
+			<SupportCards />
+
+			<h1 className='text-5xl font-serif mt-4 text-center pt-4 text-black'>
 				You can donate through
 			</h1>
 
-			<div className='grid sm:grid-cols-1 sm:justify-center sm:align-center md:grid-cols-3 text-center md:py-8 md:pr-4 sm:pl-52'>
-				<div className='col-4'>
+			<div className='grid grid-cols-3 justify-center items-center text-center py-8 md:pr-12 sm:pr-8 md:pl-12 sm:pl-4'>
+				<div className='col-span-1'>
 					<a href='https://gofund.me/bc18e7b5'>
 						<img
 							src='https://p.kindpng.com/picc/s/151-1514121_go-fund-me-hd-png-download.png'
 							alt='gofundme logo'
 							id='gofundme-logo'
-							className='hover:scale-105 sm:w-40 sm:h-30 md:h-30 md:h-40 md:pb-6 md:w-72 sm:mt-4 sm:align-center'
+							className='hover:scale-105 w-40 h-30 md:h-40 md:w-72 mx-auto mt-4'
 						/>
 					</a>
 				</div>
-
-				<div className='col-2'>
-					<p id='or-text' className='text-3xl font-serif pl-16'>
+				<div className='col-span-1'>
+					<p id='or-text' className='text-3xl font-serif'>
 						or
 					</p>
 				</div>
 
-				<div className='col-6'>
+				<div className='col-span-1 '>
 					<a href='https://www.mchanga.africa/fundraiser/44234'>
 						<img
 							src={
@@ -52,7 +53,7 @@ export const Support = () => {
 							}
 							alt='mchanga'
 							id='mchanga-logo'
-							className='hover:scale-105 w-40'
+							className='hover:scale-105 w-40 md:w-60 mx-auto'
 						/>
 					</a>
 				</div>
--- client/src/components/Support/SupportCards.jsx
+++ client/src/components/Support/SupportCards.jsx
@@ -6,25 +6,29 @@ import {BsHouseHeartFill} from 'react-icons/bs'
 
 export const SupportCards = () => {
   return (
-		<div className='grid sm:grid-cols-1 align-center justify-center me-5 mb-5 px-0 md:grid-cols-4 pt-0 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
-			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
-				<FaSchool className='icon' />
-				<p>Educating the vulnerable</p>
+		<div className='grid sm:grid-cols-1 justify-center md:gap-4 sm:gap-2 md:grid-cols-4 md:p-4 sm:p-8 sm:pt-1 sm:text-center'>
+			<div className='card shadow text-black flex flex-col items-center sm:pt-2 md:pt-4 justify-center support '>
+				<FaSchool className='icon sm:text-5xl mb-4' />
+				<p className='text-center'>Educating the vulnerable</p>
 			</div>
 
-			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
-				<GiTakeMyMoney className='icon' />
-				<p>To equip victims with skills that can help in job marketing</p>
+			<div className='card shadow text-black flex flex-col items-center sm:pt-2 md:pt-4 justify-center support '>
+				<GiTakeMyMoney className='icon sm:text-5xl mb-4' />
+				<p className='text-center'>
+					To equip victims with skills that can help in job marketing
+				</p>
 			</div>
 
-			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
-				<HiSpeakerphone className='icon' />
-				<p>Mentorship to raise awareness on mental health</p>
+			<div className='card shadow text-black flex flex-col items-center sm:pt-2 md:pt-4 justify-center support '>
+				<HiSpeakerphone className='icon text-5xl mb-4' />
+				<p className='text-center'>
+					Mentorship to raise awareness on mental health
+				</p>
 			</div>
 
-			<div className='card shadow text-black grid sm:grid-cols-1 sm:justify-center support me-5 mb-5 md:pb-8 md:pt-4 px-0 pt-60 p-4 md:ml-8 sm:ml-[-60px] sm:pl-12'>
-				<BsHouseHeartFill className='icon' />
-				<p>
+			<div className='card shadow text-black flex flex-col items-center sm:pt-2 md:pt-4 justify-center support '>
+				<BsHouseHeartFill className='icon text-5xl mb-4' />
+				<p className='text-center'>
 					Operational safehouse that rescues victims from all parts of the
 					country{' '}
 				</p>
