58f8f5725a0d004a1a212e0112a72016e7d26754
Osorabrian <osorabrian@gmail.com>
2023-05-05 23:12:18 +0300

    add pagination to blog page

--- client/src/components/BlogPage/BlogPage.jsx
+++ client/src/components/BlogPage/BlogPage.jsx
@@ -1,39 +1,109 @@
-import React, {useEffect, useState} from 'react'
-import { BlogCard } from './BlogCard'
+import React, { useEffect, useState } from 'react';
+import { BlogCard } from './BlogCard';
 import './BlogPage.css'
 
+const BLOGS_PER_PAGE = 6; // Number of blogs to display per page
+
 export const BlogPage = () => {
+	const [blogs, setBlogs] = useState([]);
+	const [search, setSearch] = useState('');
+	const [currentPage, setCurrentPage] = useState(1);
 
-  const [blogs, setBlogs] = useState([])
-  const [search, setSearch] = useState('')
+	useEffect(() => {
+		fetch('https://rebirth-ktaf.onrender.com/blogs')
+			.then((response) => response.json())
+			.then((blogData) => setBlogs(blogData));
+	}, []);
 
-  useEffect(() => {
-    fetch('https://rebirth-ktaf.onrender.com/blogs')
-    .then(response => response.json())
-    .then(blogData => setBlogs(blogData))
-  }, [])
+	const filteredBlogs = blogs.filter((blog) =>
+		blog.title.toLowerCase().includes(search.toLowerCase())
+	);
 
-  const blogData = blogs.filter((blog) => blog.title.toLowerCase().includes(search.toLowerCase()))
+	const totalBlogs = filteredBlogs.length;
+	const totalPages = Math.ceil(totalBlogs / BLOGS_PER_PAGE);
 
-  const cards = blogData.map((blog) => {
-    return(
-      <BlogCard key={blog.id.$oid} id={blog.id.$oid} title={blog.title} content={blog.body} image={blog.image_url}/>
-    )
-  })
+	// Determine the pagination range based on the current page
+	let startPage = 1;
+	let endPage = totalPages;
+	if (totalPages > 5) {
+		if (currentPage <= 3) {
+			endPage = 5;
+		} else if (currentPage >= totalPages - 2) {
+			startPage = totalPages - 4;
+		} else {
+			startPage = currentPage - 2;
+			endPage = currentPage + 2;
+		}
+	}
 
-  return (
-      <div id='blog-page'>
+	// Get the index of the first and last blog to display on the current page
+	const indexOfLastBlog = currentPage * BLOGS_PER_PAGE;
+	const indexOfFirstBlog = indexOfLastBlog - BLOGS_PER_PAGE;
 
-          <h1 className='text-5xl font-serif ' style={{textAlign: 'center', marginTop: '100px'}}>BLOGS</h1>
+	// Get the blogs to display on the current page
+	const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
 
-          <div id='search-bar'>
-            <input type='text' className='form-control' placeholder={'Search for Blog'} onChange={(e) => setSearch(e.target.value)}/>
-          </div>
+	const cards = currentBlogs.map((blog) => {
+		return (
+			<BlogCard
+				key={blog.id.$oid}
+				id={blog.id.$oid}
+				title={blog.title}
+				content={blog.body}
+				image={blog.image_url}
+			/>
+		);
+	});
 
-          <div className='row mt-3' id='cards-div'>
-            {cards}
-          </div>
-          
-        </div>    
-  )
-}
+	return (
+		<div id='blog-page' className='bg-[#CBC2F3] mt-[-140px] text-black'>
+			<h1 className='text-5xl font-serif text-center mt-5'>BLOGS</h1>
+
+			<div
+				id='search-bar'
+				className=''>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='Search for Blog'
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</div>
+
+			<div className='row ms-5 mt-3'>
+				{cards}
+			</div>
+
+			<div className='flex justify-center sm:mt-2'>
+				{currentPage > 1 && (
+					<button
+						className='px-4 py-2 mx-2 rounded-lg'
+						onClick={() => setCurrentPage(currentPage - 1)}>
+						Prev
+					</button>
+				)}
+				{Array.from(
+					{ length: endPage - startPage + 1 },
+					(_, i) => startPage + i
+				).map((page) => (
+					<button
+						key={page}
+						className={`px-4 py-2 mx-2 rounded-lg ${
+							page === currentPage ? ' bg-purple-500 text-white' : ''
+						}`}
+						onClick={() => setCurrentPage(page)}>
+						{page}
+					</button>
+				))}
+				{currentPage < totalPages && (
+					<button
+						className='px-4 py-2 mx-2 rounded-lg'
+						onClick={() => setCurrentPage(currentPage + 1)}>
+						Next
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
+
