70685fd82c14fc0c6394a9a2355d80f0bf29d855
Michelle-Mungai <81002553+Michelle-Mungai@users.noreply.github.com>
2023-05-18 20:03:59 +0300

    Update Navbar.jsx
    Original code back

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,104 +1,79 @@
-import { useState, useEffect } from 'react';
+import React, {useState} from 'react';
+import logo from '../../assets/logo.png'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import { NavLink } from 'react-router-dom';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import logo from '../../assets/logo.png';
 
-function Navbar() {
-  const [nav, setNav] = useState(false);
-
-  useEffect(() => {
-		// add scroll event listener
-		window.addEventListener('scroll', handleScroll);
-
-		// cleanup on unmount
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
-
-	const handleScroll = () => {
-		// hide navbar when scroll position is greater than 50px
-		if (window.scrollY > 50) {
-			setNav(false);
-		} else {
-			setNav(true);
-		}
+const Navbar = () => {
+	const [nav, setNav] = useState(false);
+	const handleNav = () => {
+		setNav(!nav);
 	};
-
-  function handleNav() {
-    setNav(!nav);
-  }
-
-  return (
-    <div className='text-white cursor-pointer flex justify-between items-center sticky h-24 max-w-[1240px] mx-auto px-4 z-1'>
-      <NavLink to='/'>
-        <img
-          src={logo}
-          alt='logo'
-          className='client-logo sm:w-28 md:w-20 md: md:h-30 sm:pt-12 md:pt-2'
-        />
-      </NavLink>
-      <ul className='hidden md:flex flex-wrap justify-end items-center gap-4'>
-        <NavLink to='/' className='p-4 hover:text-yellow-300 hover:font-bold'>
-          {' '}
-          Home{' '}
-        </NavLink>
-        <NavLink
-          to='/aboutus'
-          className='p-4 hover:text-yellow-300 hover:font-bold'>
-          {' '}
-          About{' '}
-        </NavLink>
-        <NavLink
-          to='/blogs'
-          className='p-4 hover:text-yellow-300 hover:font-bold'>
-          Blogs
-        </NavLink>
-        <NavLink
-          to='/programs'
-          className='p-4 hover:text-yellow-300 hover:font-bold'>
-          Programs
-        </NavLink>
-        <NavLink
-          to='/support'
-          className='p-4 hover:text-yellow-300 hover:font-bold'>
-          {' '}
-          Support{' '}
-        </NavLink>
-        <NavLink
-          to='/contact'
-          className='p-4 hover:text-yellow-300 hover:font-bold'>
-          {' '}
-          Contact{' '}
-        </NavLink>
-      </ul>
-      <div
-        onClick={handleNav}
-        className='block md:hidden hover:text-yellow-300 hover:font-bold'>
-        {nav ? <AiOutlineClose size={28} /> : <AiOutlineMenu size={28} />}
-      </div>
-      <div
-        className={
-          nav
-            ? 'fixed left-0 top-0 w-[37%] md:hidden h-full border-r-gray-900 bg-gray-600 ease-in-out duration-500'
-            : 'fixed left-[-100%]'
-        }>
-        <ul className='p-12 uppercase'>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/'>Home </NavLink>
-          </li>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/aboutus'>About </NavLink>
-          </li>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/blogs'>Blogs</NavLink>
-          </li>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/programs'>Programs </NavLink>
-          </li>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/contact'>Contact </NavLink>
-          </li>
-          <li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
-            <NavLink to='/support'>Support</NavLink>
+	return (
+		<div className='text-white cursor-pointer flex justify-between items-center font-serif sticky h-24 max-w-[1240px] mx-auto px-4' id='navbar'>
+			<img src={logo} alt='logo' className='sm:w-28 md:w-20 md: md:h-30 sm:pt-12 md:pt-2' />
+			<ul className='hidden md:flex font-serif active:font-bold active:text-yellow-300 active:text-xl'>
+				<NavLink to='/' className='p-4 hover:text-yellow-300 hover:font-bold'>
+					{' '}
+					Home{' '}
+				</NavLink>
+				<NavLink
+					to='/aboutus'
+					className='p-4 hover:text-yellow-300 hover:font-bold'>
+					{' '}
+					About{' '}
+				</NavLink>
+				<NavLink
+					to='/blogs'
+					className='p-4 hover:text-yellow-300 hover:font-bold'>
+					Blogs
+				</NavLink>
+				<NavLink
+					to='/programs'
+					className='p-4 hover:text-yellow-300 hover:font-bold'>
+					Programs
+				</NavLink>
+				<NavLink
+					to='/support'
+					className='p-4 hover:text-yellow-300 hover:font-bold'>
+					{' '}
+					Support{' '}
+				</NavLink>
+				<NavLink
+					to='/contact'
+					className='p-4 hover:text-yellow-300 hover:font-bold'>
+					{' '}
+					Contact{' '}
+				</NavLink>
+			</ul>
+			<div
+				onClick={handleNav}
+				className='block md:hidden hover:text-yellow-300 hover:font-bold'>
+				{!nav ? <AiOutlineClose size={28} /> : <AiOutlineMenu size={28} />}
+			</div>
+			<div
+				className={
+					!nav
+						? 'fixed left-0 top-0 w-[37%] md:hidden h-full border-r-gray-900 bg-gray-600 ease-in-out duration-500'
+						: 'fixed left-[-100%]'
+				}>
+				<ul className='p-12 uppercase'>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/'>Home </NavLink>
+					</li>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/aboutus'>About </NavLink>
+					</li>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/blogs'>Blogs</NavLink>
+					</li>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/programs'>Programs </NavLink>
+					</li>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/contact'>Contact </NavLink>
+					</li>
+					<li className='p-4 border-b border-gray-900 hover:text-yellow-300 hover:font-bold'>
+						<NavLink to='/support'>Support</NavLink>
 					</li>
 				</ul>
 			</div>
